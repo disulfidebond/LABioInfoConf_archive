31240f7dceb5ade24a5edc30edc129c3c8e048f9
disulfidebond <disulfidebond@gmail.com>
2015-03-01 17:38:29 -0600

    Initial Commit

---
+++ LABioInfoConf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LABioInfoConf
+//
+//  Created by John on 3/1/15.
+//  Copyright (c) 2015 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LABioInfoConf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LABioInfoConf
+//
+//  Created by John on 3/1/15.
+//  Copyright (c) 2015 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
